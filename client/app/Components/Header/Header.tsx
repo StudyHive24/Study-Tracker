16371c51bad012cc69e58511cf1d7723e1576316
dextermadh <madhukaabhishek30@gmail.com>
2024-10-12 18:21:40 +0530

    'v1'

---
+++ client/app/Components/Header/Header.tsx
@@ -0,0 +1,43 @@
+"use client"
+import { useUserContext } from '@/context/userContext'
+import React from 'react'
+
+function Header() {
+
+  const {user} = useUserContext()
+
+  const { name } = user
+
+  const userId = user._id
+
+  return (
+    <header className="px-6 my-4 w-full flex items-center justify-between bg-[#f9f9f9]">
+      <div>
+        <h1> 
+           <span role='img' aria-label='wave'>👋</span>
+           {userId ? `Welcome, ${name} to StudyHive` : "Welcome to StudyHive"}
+        </h1>
+        <p className='text-sm '>
+          {userId ? (
+            <>You have <span className='font-bold text-[#3aafae]'>5</span> active tasks
+            </>
+          ) : (
+            "Please login or register to view your tasks"
+          )
+        
+        }
+        </p>
+      </div>
+      <div className='h-[50px] flex items-center gap-[10.4rem]'>
+        <button 
+          className='px-8 py-3 bg-[#3aafae] text-white rounded-[50px]
+          hover:bg-[#00A1F1] hover:text-white transition-all duration-200 ease-in-out'
+        >
+          Create a new Task
+        </button>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ client/providers/SidebarProvider.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function SidebarProvider() {
+  return <div>SidebarProvider</div>
+}
+
+export default SidebarProvider
