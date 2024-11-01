2f3cd1a52900fd9ca4dbced935eab86fe0eb2a76
yahan <yahan8000@gmail.com>
2024-11-01 11:54:26 +0530

    remove_sidebar_from_settings

--- client/app/settings/page.tsx
+++ client/app/settings/page.tsx
@@ -1,10 +1,8 @@
-import { SideBar } from "../Components/Sidebar.tsx/Sidebar";
 
 // app/settings/page.tsx
 export default function Settings() {
     return (
         <div className="flex">
-            <SideBar />
             <div className="flex-1 p-4">
                 <h1>Settings</h1>
                 <p>This is the settings page content.</p>
