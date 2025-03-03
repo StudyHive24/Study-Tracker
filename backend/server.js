83ae1e7c5015185091095f6cd0eeec7f9a787e5e
dextermadh <93965850+dextermadh@users.noreply.github.com>
2025-03-03 23:02:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'https://studyhiveouslf6.vercel.app', credentials: true }));
 app.use(cookieParser())
 
 const port = process.env.PORT;
