d001158e285f8d844cf962de103d8fa19a141ec0
dextermadh <93965850+dextermadh@users.noreply.github.com>
2025-03-03 22:52:37 +0530

    added server url

--- client/context/taskContext.js
+++ client/context/taskContext.js
@@ -6,7 +6,7 @@ import toast from 'react-hot-toast'
 
 const TaskContext = createContext()
 
-const serverUrl = 'http://localhost:8000'
+const serverUrl = 'https://study-hive-server-f6.vercel.app/'
 
 export const TasksProvider = ({children}) => {
 
--- client/context/userContext.js
+++ client/context/userContext.js
@@ -11,7 +11,7 @@ const UserContext = React.createContext()
 axios.defaults.withCredentials = true
 
 export const UserContextProvider = ({children}) => {
-    const serverUrl = 'http://localhost:8000'
+    const serverUrl = 'https://study-hive-server-f6.vercel.app/'
 
     const router = useRouter()
 
