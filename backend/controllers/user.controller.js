c2312b8cae534b717f99df1340fac9fa372efc00
dextermadh <93965850+dextermadh@users.noreply.github.com>
2025-03-05 03:33:42 +0530

    Update user.controller.js

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -78,6 +78,7 @@ export const registerUser = asyncHandler(async (req, res) => {
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       sameSite: 'None',
       secure: false,
+      domain: 'study-hive-server-f6.vercel.app'
     });
 
     if (user) {
@@ -140,6 +141,7 @@ export const loginUser = asyncHandler(async (req, res) => {
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
       sameSite: 'None',
       secure: true,
+      domain: 'study-hive-server-f6.vercel.app'
     });
 
     // send back tot the user and token in the response to the client
